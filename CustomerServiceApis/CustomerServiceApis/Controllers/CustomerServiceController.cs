25af65f96fa1ebca20872040eb1b31fc9b4e847c
Xiang Yan <xiangyan@microsoft.com>
2017-02-16 10:12:27 -0800

    add customerservice apis to make the demo work

---
+++ CustomerServiceApis/CustomerServiceApis/Controllers/CustomerServiceController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.SqlClient;
+using CustomerServiceApis.Models;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CustomerServiceApis.Controllers
+{
+    public class CustomerServiceController : ApiController
+    {
+        private const string _bingmapappid = "_YourBingMapId_";
+        private const string _bingmapstaticmapwithrouteurl = "http://dev.virtualearth.net/REST/v1/Imagery/Map/Road/{4}%2C{5}/15/Routes/Walking?waypoint.1={2},{3};64;{6}&waypoint.2={0},{1};64;{7}&mapSize=400,300&format=png&key=" + _bingmapappid;
+        private const string _bingmapstaticmapwith1pointsurl = "http://dev.virtualearth.net/REST/V1/Imagery/Map/Road/{0}%2C{1}/13?mapSize=400,300&format=png&pushpin={0},{1};64;{2}&key=" + _bingmapappid;
+        private const string _bingmappointtoaddrurl = "http://dev.virtualearth.net/REST/v1/Locations/{0},{1}?o=xml&key=" + _bingmapappid;
+
+        [Route("getavailableemployee")]
+        [HttpGet]
+        public Employee GetAvailableEmployee()
+        {
+            //get available employee from database
+            return null;
+        }
+
+        [Route("getaddress")]
+        [HttpGet]
+        public string GetAddress(double latitude, double longitude)
+        {
+            string requesturl = string.Format(_bingmappointtoaddrurl, latitude.ToString(), longitude.ToString());
+            XmlDocument response = GetXmlResponse(requesturl);
+            if (response == null)
+            {
+                return "";
+            }
+            var addresses = response.GetElementsByTagName("FormattedAddress");
+            string ret = "";
+            if (addresses.Count > 0)
+            {
+                ret = addresses[0].InnerText.ToString();
+            }
+            return ret;
+        }
+
+        [Route("getinformation")]
+        [HttpGet]
+        public Employee GetInformation(string id)
+        {
+            //load employee information from database
+            return null;
+        }
+
+        [Route("register")]
+        [HttpPost]
+        public void Register([FromBody]Employee value)
+        {
+            //register employee into database
+        }
+
+        [Route("saveinformation")]
+        [HttpPost]
+        public void SaveInformation([FromBody]Employee value)
+        {
+            //save employee information into database
+        }
+
+        [Route("filecase")]
+        [HttpGet]
+        public string FileCase(string userid, string incidenttype, double lat, double lon)
+        {
+            //add record to database
+            return "12345";
+        }
+
+        [Route("locatebike")]
+        [HttpGet]
+        public GeoLocation LocateBike(string bikeid, string datetime = "")
+        {
+            GeoLocation ret = new GeoLocation();
+            if (datetime == "")
+            {
+                ret.latitude = 40.722567290067673;
+                ret.longitude = -73.9976117759943;
+                ret.name = "Chipotle Mexican Grill";
+            }
+            else
+            {
+                ret.latitude = 40.720725953578949;
+                ret.longitude = -74.005964174866676;
+                ret.name = "Spring Studios";
+            }
+            return ret;
+        }
+
+        [Route("getmapwithroute")]
+        [HttpGet]
+        public string GetMapWithRoute(double latitude1, 
+            double longitude1, string name1, double latitude2,
+            double longitude2, string name2)
+        {
+            double midlat = (latitude1 + latitude2) / 2;
+            double midlon = (longitude1 + longitude2) / 2;
+            return string.Format(_bingmapstaticmapwithrouteurl,
+                       latitude1.ToString(), longitude1.ToString(),
+                       latitude2.ToString(), longitude2.ToString(),
+                       midlat.ToString(), midlon.ToString(), name1.ToLower(),
+                       name2.ToLower());
+        }
+
+        [Route("getmapwith1pin")]
+        [HttpGet]
+        public string GetMapWith1Pin(double latitude,
+            double longitude, string name)
+        {
+            return string.Format(_bingmapstaticmapwith1pointsurl,
+                       latitude.ToString(), longitude.ToString(), name);
+        }
+
+        [Route("geteta")]
+        [HttpGet]
+        public async Task<int> GetETA(double latitude1, double longitude1, 
+            double latitude2, double longitude2)
+        {
+            return 15;
+        }
+
+        private static XmlDocument GetXmlResponse(string requestUrl)
+        {
+            try
+            {
+                HttpWebRequest request = WebRequest.Create(requestUrl) as HttpWebRequest;
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(response.GetResponseStream());
+                return (xmlDoc);
+
+            }
+            catch (Exception e)
+            {
+                var roo = e.Message;
+                return null;
+            }
+        }
+    }
+}
---
+++ CustomerServiceApis/CustomerServiceApis/Models/BingMapRouteData.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomerServiceApis.Models
+{
+    public class Rootobject
+    {
+        public string authenticationResultCode { get; set; }
+        public string brandLogoUri { get; set; }
+        public string copyright { get; set; }
+        public Resourceset[] resourceSets { get; set; }
+        public int statusCode { get; set; }
+        public string statusDescription { get; set; }
+        public string traceId { get; set; }
+    }
+
+    public class Resourceset
+    {
+        public int estimatedTotal { get; set; }
+        public Resource[] resources { get; set; }
+    }
+
+    public class Resource
+    {
+        public string __type { get; set; }
+        public float[] bbox { get; set; }
+        public string id { get; set; }
+        public string distanceUnit { get; set; }
+        public string durationUnit { get; set; }
+        public Routeleg[] routeLegs { get; set; }
+        public string trafficCongestion { get; set; }
+        public string trafficDataUsed { get; set; }
+        public float travelDistance { get; set; }
+        public int travelDuration { get; set; }
+        public int travelDurationTraffic { get; set; }
+    }
+
+    public class Routeleg
+    {
+        public Actualend actualEnd { get; set; }
+        public Actualstart actualStart { get; set; }
+        public object[] alternateVias { get; set; }
+        public int cost { get; set; }
+        public string description { get; set; }
+        public Itineraryitem[] itineraryItems { get; set; }
+        public string routeRegion { get; set; }
+        public Routesubleg[] routeSubLegs { get; set; }
+        public float travelDistance { get; set; }
+        public int travelDuration { get; set; }
+    }
+
+    public class Actualend
+    {
+        public string type { get; set; }
+        public float[] coordinates { get; set; }
+    }
+
+    public class Actualstart
+    {
+        public string type { get; set; }
+        public float[] coordinates { get; set; }
+    }
+
+    public class Itineraryitem
+    {
+        public string compassDirection { get; set; }
+        public Detail[] details { get; set; }
+        public string exit { get; set; }
+        public string iconType { get; set; }
+        public Instruction instruction { get; set; }
+        public Maneuverpoint maneuverPoint { get; set; }
+        public string sideOfStreet { get; set; }
+        public string tollZone { get; set; }
+        public string towardsRoadName { get; set; }
+        public string transitTerminus { get; set; }
+        public float travelDistance { get; set; }
+        public int travelDuration { get; set; }
+        public string travelMode { get; set; }
+        public Warning[] warnings { get; set; }
+        public Hint[] hints { get; set; }
+    }
+
+    public class Instruction
+    {
+        public object formattedText { get; set; }
+        public string maneuverType { get; set; }
+        public string text { get; set; }
+    }
+
+    public class Maneuverpoint
+    {
+        public string type { get; set; }
+        public float[] coordinates { get; set; }
+    }
+
+    public class Detail
+    {
+        public int compassDegrees { get; set; }
+        public int[] endPathIndices { get; set; }
+        public string maneuverType { get; set; }
+        public string mode { get; set; }
+        public string[] names { get; set; }
+        public string roadType { get; set; }
+        public int[] startPathIndices { get; set; }
+        public string[] locationCodes { get; set; }
+    }
+
+    public class Warning
+    {
+        public string origin { get; set; }
+        public string severity { get; set; }
+        public string text { get; set; }
+        public string to { get; set; }
+        public string warningType { get; set; }
+    }
+
+    public class Hint
+    {
+        public string hintType { get; set; }
+        public string text { get; set; }
+    }
+
+    public class Routesubleg
+    {
+        public Endwaypoint endWaypoint { get; set; }
+        public Startwaypoint startWaypoint { get; set; }
+        public float travelDistance { get; set; }
+        public int travelDuration { get; set; }
+    }
+
+    public class Endwaypoint
+    {
+        public string type { get; set; }
+        public float[] coordinates { get; set; }
+        public string description { get; set; }
+        public bool isVia { get; set; }
+        public string locationIdentifier { get; set; }
+        public int routePathIndex { get; set; }
+    }
+
+    public class Startwaypoint
+    {
+        public string type { get; set; }
+        public float[] coordinates { get; set; }
+        public string description { get; set; }
+        public bool isVia { get; set; }
+        public string locationIdentifier { get; set; }
+        public int routePathIndex { get; set; }
+    }
+}
